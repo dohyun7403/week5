903a27a73913a3ae6b10cedf6c7cf12dd7179538
jin730id <qkrwlsdn730@gmail.com>
2024-10-07 16:17:50 +0900

    dd

---
+++ src/Question1.java
@@ -0,0 +1,19 @@
+public class Question1 {
+    public static void main(String[] args) {
+        for(int i=1; i<=9; i++){
+            System.out.println("7 * "+i+" = "+7*i);
+        }
+    }
+}
+//public class Question1 {
+//    public static void main(String[] args) {
+//        for(int i=2; i<=9; i++){
+//            for(int j=1; j<=9; j++){
+//                System.out.print(i+" * "+j+" = "+j*i +"\t");
+//            }
+//            System.out.println("");
+//
+//        }
+//    }
+//}
+// 구구단 전체 출력
